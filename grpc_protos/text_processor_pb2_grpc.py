21a175482d05a4055fab89f348e815a8bb31d18e
shvdev1 <shevelev4218@yandex.ru>
2025-12-11 22:31:04 +0300

    added build dependencies

--- grpc_protos/text_processor_pb2_grpc.py
+++ grpc_protos/text_processor_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import text_processor_pb2 as text__processor__pb2
+from grpc_protos import text_processor_pb2 as text__processor__pb2
 
 GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
